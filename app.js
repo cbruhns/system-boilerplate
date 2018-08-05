1d387fe5c01bf3bcad7628104c6b65144cd01952
cbruhns <cameron.bruhns@gmail.com>
2018-08-05 14:40:37 -0400

    process edit

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var LocalStrategy = require('passport-local').Strategy;
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
 
-mongoose.connect(proess.env.MONGOPATH,proess.env.MONGOUSER,proess.env.MONGOPASS);
+mongoose.connect(process.env.MONGOPATH,process.env.MONGOUSER,process.env.MONGOPASS);
 var db = mongoose.connection;
 
 var routes = require('./routes/index');
